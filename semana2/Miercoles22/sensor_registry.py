b9553b01ac9afb5f3e40062516894826b58af33a
Julian <jose.julian.14.04@gmail.com>
2026-07-25 19:04:21 -0600

    refactor: extraer validacion en SensorRegistry

--- semana2/Miercoles22/sensor_registry.py
+++ semana2/Miercoles22/sensor_registry.py
@@ -3,9 +3,12 @@ class SensorNotFoundError(Exception):
 
 class SensorRegistry:
     def __init__(self):
-        self.sensors = {}
+        self._sensors = {}
+
+    def _validar_existencia(self, sensor_id):
+        if sensor_id not in self._sensors:
+            raise SensorNotFoundError(f"Sensor {sensor_id} no encontrado")
 
     def get(self, sensor_id):
-        if sensor_id not in self.sensors:
-            raise SensorNotFoundError(f"Sensor {sensor_id} no encontrado")
-        return self.sensors[sensor_id]
+        self._validar_existencia(sensor_id)
+        return self._sensors[sensor_id]
